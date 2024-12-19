5f43a7b24b1197e5e9099dccf61e7556894d9da9
Yutong Zhang <zhangyt2000@gmail.com>
2024-12-19 12:00:33 -0700

    Generate some waypoint evaluation

--- src/eval_waypoint.py
+++ src/eval_waypoint.py
@@ -15,30 +15,34 @@ waypoint_util.load_pretrain('checkpoint/waypoint_net.ckpt')
 env = gym.make('gym_pusht/PushT-v0', render_mode='rgb_array')
 env = env.unwrapped
 
-# get first observation
-obs, info = env.reset()
-start = env.render()
-plt.figure()
-plt.imshow(start)
-plt.show()
+for test in range(10):
+    # get first observation
+    obs, info = env.reset()
+    # get rid of the agent
+    env.agent.position = [0, 0]
+    obs, reward, terminated, truncated, info = env.step(np.zeros(2))
+    start = env.render()
 
-for _ in range(10):
-    # get goal from waypoint network
-    obs_input = normalize_data(torch.tensor(obs).float(),
-                               waypoint_util.dataset.stats['obs'])
-    waypoint = waypoint_util.waypoint_net(obs_input[2:])
-    waypoint = waypoint.detach().numpy()
-    waypoint_full = np.zeros((5))
-    waypoint_full[2:] = waypoint
-    waypoint_full = unnormalize_data(waypoint_full, waypoint_util.dataset.stats['obs'])
-
-    # override T position
-    env.block.position = list(waypoint_full[2:4])
-    env.block.angle = waypoint_full[4]
-    # step the environment without moving the agent
-    obs, reward, terminated, truncated, info = env.step(env.agent.position)
-
-    state = env.render()
     plt.figure()
-    plt.imshow(state)
-    plt.show()
+    plt.imshow(start)
+    plt.savefig(f'./img/waypoint/waypoint_{test}_0')
+
+    for step in range(10):
+        # get goal from waypoint network
+        obs_input = normalize_data(torch.tensor(obs).float(),
+                                   waypoint_util.dataset.stats['obs'])
+        waypoint = waypoint_util.waypoint_net(obs_input[2:]).detach().numpy()
+        waypoint_full = np.zeros((5))
+        waypoint_full[2:] = waypoint
+        waypoint_full = unnormalize_data(waypoint_full, waypoint_util.dataset.stats['obs'])
+
+        # override T position
+        env.block.position = list(waypoint_full[2:4])
+        env.block.angle = waypoint_full[4]
+        # step the environment without moving the agent
+        obs, reward, terminated, truncated, info = env.step(np.zeros(2))
+
+        state = env.render()
+        plt.figure()
+        plt.imshow(state)
+        plt.savefig(f'./img/waypoint/waypoint_{test}_{step+1}')
